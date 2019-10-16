9a769b2aac1608fb88c5a0c5ab4372b636305e08
Joshua Minor <joshm@pixar.com>
2019-10-16 14:45:25 -0700

    Easier access to pipe character.

--- keyboards/redox/keymaps/jminor/keymap.c
+++ keyboards/redox/keymaps/jminor/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      RESET   ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_DEL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     TERM_ON ,KC_EXLM ,KC_AT   ,KC_LCBR ,KC_RCBR ,KC_PIPE ,KC_LSFT ,                          KC_RSFT ,KC_QUOT ,KC_UNDS ,KC_MINS ,KC_EQL  ,KC_PLUS ,KC_BSLS ,
+     TERM_ON ,KC_EXLM ,KC_AT   ,KC_LCBR ,KC_RCBR ,KC_PIPE ,KC_LSFT ,                          KC_RSFT ,KC_QUOT ,KC_UNDS ,KC_MINS ,KC_EQL  ,KC_PLUS ,KC_PIPE ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      TERM_OFF,KC_HASH ,KC_DLR  ,KC_LBRC ,KC_RBRC ,KC_GRV  ,KC_PREV ,                          KC_NEXT, XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_QUOT ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
